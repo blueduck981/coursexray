d5aaea1c43c31ee65ea0e924f6ff8fa9aa793ce5
blueduck981 <blueduck981@gmail.com>
2017-02-11 19:13:56 -0800

    fixed errors on unidentified keywords

--- flaskexample/views.py
+++ flaskexample/views.py
@@ -29,7 +29,12 @@ tfidf_description = pickle.load(open("data/description_tfidf.dat","rb"))
 hold_data = pickle.load(open("data/proc_info.pkl","rb"))
 query_results = pickle.load(open("data/course_info.pkl","rb"))
 #reviews = pickle.load(open("data/reviews.pkl","rb"))
-
+#Collect and process reviews. Now in pickle form.
+#collect_docs, collect_decs, collect_url_order,collect_course_sentiment=initial_model.initial_processing(query_results, reviews)
+collect_docs=hold_data.proc_reviews
+collect_decs=hold_data.proc_desc
+collect_url_order=hold_data.course_url
+collect_course_sentiment=hold_data.sentiment
 
 @app.route('/')
 @app.route('/index')
@@ -73,13 +78,6 @@ def xray_output():
   df2=query_results.set_index('url')
 
 
-  #Collect and process reviews. Now in pickle form.
-  #collect_docs, collect_decs, collect_url_order,collect_course_sentiment=initial_model.initial_processing(query_results, reviews)
-  collect_docs=hold_data.proc_reviews
-  collect_decs=hold_data.proc_desc
-  collect_url_order=hold_data.course_url
-  collect_course_sentiment=hold_data.sentiment
-
   #Take user input, format and stem for analysis
   p_stemmer = PorterStemmer()
   keywords = request.args.get('user_keyword')
@@ -87,125 +85,147 @@ def xray_output():
 
   input_words=[input_string]
   stemmed_input = [p_stemmer.stem(i) for i in jc.token_ws(input_string)]
-  input_words=[' '.join(stemmed_input)]
   input_array= [i for i in jc.token_ws(input_string)]
 
-  #Store the search results for use in the Xray
-  with open('data/store_keys.dat', 'wb') as outfile:
-    pickle.dump(input_words, outfile, pickle.HIGHEST_PROTOCOL)
+  good_input=input_array
+  stemmed_input, bad_output,good_input, error_list=initial_model.check_input(stemmed_input,\
+          reviews_vectorizer,description_vectorizer, good_input )
 
-  with open('data/store_unstem.dat', 'wb') as outfile:
-    pickle.dump(input_string, outfile, pickle.HIGHEST_PROTOCOL)  
+  input_words=[' '.join(stemmed_input)]
+  print input_words
+  #print input_string
+  save_string= ' '.join(good_input)
+  print save_string
+  if input_words[0]=='':
+        #print str(list(keep_error))
+        #bad_output= ", ".join(keep_error)
+
+        return render_template("oops.html", the_result = bad_output)
+  else:
+
+  #Store the search results for use in the Xray
+    with open('data/store_keys.dat', 'wb') as outfile:
+      pickle.dump(input_words, outfile, pickle.HIGHEST_PROTOCOL)
+
+    with open('data/store_unstem.dat', 'wb') as outfile:
+      pickle.dump(save_string, outfile, pickle.HIGHEST_PROTOCOL)  
 
   #calculate similarities using input and review&description matrices
-  review_cosine_scores=initial_model.get_cosine_similarities(input_words, reviews_vectorizer, tfidf_reviews)
-  description_cosine_scores=initial_model.get_cosine_similarities(input_words, description_vectorizer, tfidf_description)
+    review_cosine_scores=initial_model.get_cosine_similarities(input_words, reviews_vectorizer, tfidf_reviews)
+    description_cosine_scores=initial_model.get_cosine_similarities(input_words, description_vectorizer, tfidf_description)
 
   #print len(collect_url_order), len(collect_course_sentiment), len(description_cosine_scores)
 
   #create a dataframe with results and add other information
-  matching_matrix= pd.DataFrame({'course_url':collect_url_order,'review_score':review_cosine_scores, 
+    matching_matrix= pd.DataFrame({'course_url':collect_url_order,'review_score':review_cosine_scores, 
                               'description_score':description_cosine_scores,'sentiment':collect_course_sentiment})
 
-  matching_matrix['total_score'] = matching_matrix.apply(lambda row:row.review_score+row.description_score,axis=1)
-  matching_matrix_sort=matching_matrix.sort_values(by='total_score',ascending=False).reset_index(inplace=False,drop=True)
+    matching_matrix['total_score'] = matching_matrix.apply(lambda row:row.review_score+row.description_score,axis=1)
+    matching_matrix_sort=matching_matrix.sort_values(by='total_score',ascending=False).reset_index(inplace=False,drop=True)
 
 
-  matching_matrix_sort['title'] = matching_matrix_sort.course_url.apply(lambda x:df2.loc[x,'format_title'])
-  matching_matrix_sort['company'] = matching_matrix_sort.course_url.apply(lambda x:df2.loc[x,'company'])
-  matching_matrix_sort['num_rev'] = matching_matrix_sort.course_url.apply(lambda x:df2.loc[x,'num_rev'])
-  matching_matrix_sort['description'] = matching_matrix_sort.course_url.apply(lambda x:df2.loc[x,'format_desc'])
-  matching_matrix_sort['short_description'] = matching_matrix_sort.course_url.apply(lambda x:df2.loc[x,'short_desc'])
+    matching_matrix_sort['title'] = matching_matrix_sort.course_url.apply(lambda x:df2.loc[x,'format_title'])
+    matching_matrix_sort['company'] = matching_matrix_sort.course_url.apply(lambda x:df2.loc[x,'company'])
+    matching_matrix_sort['num_rev'] = matching_matrix_sort.course_url.apply(lambda x:df2.loc[x,'num_rev'])
+    matching_matrix_sort['description'] = matching_matrix_sort.course_url.apply(lambda x:df2.loc[x,'format_desc'])
+    matching_matrix_sort['short_description'] = matching_matrix_sort.course_url.apply(lambda x:df2.loc[x,'short_desc'])
 
   #save the best url for the viz
-  besturl=matching_matrix_sort.loc[0,'course_url']
+    besturl=matching_matrix_sort.loc[0,'course_url']
 
 
    
   #find the scores for the different keywords in both reviews and descriptions
-  keep_r_word, keep_r_value=initial_model.get_word_power(besturl,collect_url_order,tfidf_reviews,reviews_vectorizer, stemmed_input)
-  keep_d_word, keep_d_value=initial_model.get_word_power(besturl,collect_url_order,tfidf_description,description_vectorizer, stemmed_input)
+    keep_r_word, keep_r_value=initial_model.get_word_power(besturl,collect_url_order,tfidf_reviews,reviews_vectorizer, stemmed_input)
+    keep_d_word, keep_d_value=initial_model.get_word_power(besturl,collect_url_order,tfidf_description,description_vectorizer, stemmed_input)
   
-  power_r=pd.DataFrame({'stemmed_word':keep_r_word, 'whole_word':input_array, 'review_val':keep_r_value, 'description_val':keep_d_value})
+    power_r=pd.DataFrame({'stemmed_word':keep_r_word, 'whole_word':good_input, 'review_val':keep_r_value, 'description_val':keep_d_value})
 
   #power_r=pd.DataFrame({'word':keep_r_word, 'review_val':keep_r_value, 'description_val':keep_d_value})
   
-  power_r['total'] = power_r.apply(lambda row:row.review_val+row.description_val,axis=1)
-  power_results = power_r.sort_values(by='total',ascending=False).reset_index(inplace=False,drop=True)
+    power_r['total'] = power_r.apply(lambda row:row.review_val+row.description_val,axis=1)
+    power_results = power_r.sort_values(by='total',ascending=False).reset_index(inplace=False,drop=True)
 
 
   
 
   #Pull out top 5
-  courses = []
-  for i in range(1,5):
-      courses.append(dict(url=matching_matrix_sort.iloc[i]['course_url'], 
-        coursename=matching_matrix_sort.iloc[i]['title'], description=matching_matrix_sort.iloc[i]['short_description'],
-        company=matching_matrix_sort.iloc[i]['company'], number= matching_matrix_sort.iloc[i]['num_rev'] ))
-      the_result = ''
+    courses = []
+    for i in range(1,5):
+        courses.append(dict(url=matching_matrix_sort.iloc[i]['course_url'], 
+          coursename=matching_matrix_sort.iloc[i]['title'], description=matching_matrix_sort.iloc[i]['short_description'],
+          company=matching_matrix_sort.iloc[i]['company'], number= matching_matrix_sort.iloc[i]['num_rev'] ))
+    the_result = ''
 
 
   #Pull out number 1 separately
 
-  c1 = []
+    c1 = []
   
-  c1.append(dict(url=matching_matrix_sort.loc[0,'course_url'], 
-        coursename=matching_matrix_sort.loc[0,'title'], description=matching_matrix_sort.loc[0,'short_description'],
-        company=matching_matrix_sort.loc[0,'company'], number= matching_matrix_sort.loc[0,'num_rev'] ))
+    c1.append(dict(url=matching_matrix_sort.loc[0,'course_url'], 
+          coursename=matching_matrix_sort.loc[0,'title'], description=matching_matrix_sort.loc[0,'short_description'],
+          company=matching_matrix_sort.loc[0,'company'], number= matching_matrix_sort.loc[0,'num_rev'] ))
 
    #save in a convenient format for D3
-  power = []
-  for i in range(0,5):
-    if i < len(power_results.whole_word):
-      power.append(dict( 
-        word=power_results.iloc[i]['whole_word'], 
-        rev_pow=power_results.iloc[i]['review_val'], 
-        des_pow=power_results.iloc[i]['description_val'] ))
-    else:
+    power = []
+    for i in range(0,5):
+      if i < len(power_results.whole_word):
         power.append(dict( 
-        word='', 
-        rev_pow=0, 
-        des_pow=0))
+          word=power_results.iloc[i]['whole_word'], 
+          rev_pow=power_results.iloc[i]['review_val'], 
+          des_pow=power_results.iloc[i]['description_val'] ))
+      else:
+          power.append(dict( 
+          word='', 
+          rev_pow=0, 
+          des_pow=0))
   
   #d=list(collect_course_sentiment)
  
 
   # for the Xray take reviews from best course
-  cat_rev=[]
-  take_rev=hold_data[hold_data.course_url==besturl].reviews
-  for item in take_rev:
-        cat_rev.append(item)
+    cat_rev=[]
+    take_rev=hold_data[hold_data.course_url==besturl].reviews
+    for item in take_rev:
+          cat_rev.append(item)
 
-  keyword=power_results.whole_word[0]
-  print keyword
+    keyword=power_results.whole_word[0]
+    print keyword
   
   # calculate sentiment for keyword sentences
-  keep_polarity=[]
-  sentences=tokenize.sent_tokenize(str(cat_rev).decode('utf-8'))
-  for sentence in sentences:
-      if keyword in sentence:
-          blob=TextBlob(sentence)
-          keep_polarity.append(blob.sentiment.polarity)      
+    keep_polarity=[]
+    sentences=tokenize.sent_tokenize(str(cat_rev).decode('utf-8'))
+    for sentence in sentences:
+        if keyword in sentence:
+            blob=TextBlob(sentence)
+            keep_polarity.append(blob.sentiment.polarity)      
 
 
   #helps with binning rounding
-  k=[x-0.01 for x in keep_polarity]
+    k=[x-0.01 for x in keep_polarity]
 
-  d=list(k)
+    d=list(k)
 
 
   #the_result = keywords.split(', ')
-  the_result = input_array
+    the_result = input_array
 
   #pretty text output
-  if len(the_result)==1:
-    wordstring=( ' '.join( the_result))
-  elif len(the_result)==2:
-    wordstring=( ' and '.join( the_result)) 
-  else:
-    the_result[len(the_result)-1]='and '+str(the_result[len(the_result)-1])
-    wordstring=( ", ".join( the_result)) 
-  return render_template("output.html", courses = courses, the_result = wordstring,power=power,c1=c1,dhist=d,keyword=keyword)
+    if len(the_result)==1:
+      wordstring=( ' '.join( the_result))
+    elif len(the_result)==2:
+      wordstring=( ' and '.join( the_result)) 
+    else:
+      the_result[len(the_result)-1]='and '+str(the_result[len(the_result)-1])
+      wordstring=( ", ".join( the_result)) 
+
+
+    if bad_output=='':
+      print 'all ok'
+      return render_template("output.html", courses = courses, the_result = wordstring,power=power,c1=c1,dhist=d,keyword=keyword)
+    else:
+      print 'some bad output'
+      return render_template("error_output.html", courses = courses, the_result = wordstring,power=power,c1=c1,dhist=d,keyword=keyword,bad_output=bad_output)
 
 #####################################
 
@@ -223,27 +243,14 @@ def xray_output2():
   df2=query_results.set_index('url')
 
 
-  #Collect and process reviews. Now in pickle form.
-  #collect_docs, collect_decs, collect_url_order,collect_course_sentiment=initial_model.initial_processing(query_results, reviews)
-  collect_docs=hold_data.proc_reviews
-  collect_decs=hold_data.proc_desc
-  collect_url_order=hold_data.course_url
-  collect_course_sentiment=hold_data.sentiment
-
-
   #format input
   p_stemmer = PorterStemmer()
  
   input_string= ' '.join(old_data)#.replace(',',' ')
 
   input_string_pretty= old_data_pretty
-  #input_words=old_data
-  #print input_string_pretty
-
   stemmed_input = [p_stemmer.stem(i) for i in jc.token_ws(input_string)]
   input_words=[' '.join(stemmed_input)]
-
-
   input_array= jc.token_ws(input_string_pretty)
   #print input_array
 
@@ -275,6 +282,7 @@ def xray_output2():
   keep_r_word, keep_r_value=initial_model.get_word_power(besturl,collect_url_order,tfidf_reviews,reviews_vectorizer, stemmed_input)
   keep_d_word, keep_d_value=initial_model.get_word_power(besturl,collect_url_order,tfidf_description,description_vectorizer, stemmed_input)
   
+  print keep_r_word, input_array
   power_r=pd.DataFrame({'stemmed_word':keep_r_word, 'whole_word':input_array, 'review_val':keep_r_value, 'description_val':keep_d_value})
 
 #  power_r=pd.DataFrame({'word':keep_r_word, 'review_val':keep_r_value, 'description_val':keep_d_value})
@@ -357,7 +365,7 @@ def xray_output2():
   else:
     the_result[len(the_result)-1]='and '+str(the_result[len(the_result)-1])
     wordstring=( ", ".join( the_result)) 
-  return render_template("output_xray.html", courses = courses, the_result = wordstring,power=power,c1=c1,dhist=k,keyword=keywords2)
+  return render_template("output.html", courses = courses, the_result = wordstring,power=power,c1=c1,dhist=k,keyword=keywords2)
 
 
 
